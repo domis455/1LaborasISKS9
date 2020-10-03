e75e5cab556ddf1391ffa4b5342e06461f0ed07b
domis455 <72032371+domis455@users.noreply.github.com>
2020-10-03 18:44:07 +0300

    update

---
+++ app/src/main/java/com/example/a1laborasisks9/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a1laborasisks9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+    this.tvMain.setText("Text was changed");
+
+    }
+}
