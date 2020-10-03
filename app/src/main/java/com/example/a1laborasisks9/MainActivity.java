31fb3fcbc7767fb33a981750e0a20cd5791d23db
domis455 <72032371+domis455@users.noreply.github.com>
2020-10-03 19:56:10 +0300

    update

--- app/src/main/java/com/example/a1laborasisks9/MainActivity.java
+++ app/src/main/java/com/example/a1laborasisks9/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.a1laborasisks9;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
-import android.service.autofill.TextValueSanitizer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity{
 
     private TextView tvMain;
+    private Button btnChangeColor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
+        btnChangeColor = (Button) findViewById(R.id.btnChangeColor);
     }
 
     public void onBtnChangeTextClick(View view) {
     this.tvMain.setText("Text was changed");
+    }
 
+    public void onBtnChangeTextColor(View arg0) {
+        tvMain.setTextColor(Color.RED);
     }
 }
